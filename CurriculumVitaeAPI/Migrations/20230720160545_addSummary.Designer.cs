45cc7b2efd3914bbadf72b6469c3e575c7f43e61
Woler <hristo_den@abv.bg>
2023-07-20 19:12:36 +0300

    added summary

--- CurriculumVitaeAPI/Migrations/20230720160545_addSummary.Designer.cs
+++ CurriculumVitaeAPI/Migrations/20230720160545_addSummary.Designer.cs
@@ -4,6 +4,7 @@ using CurriculumVitaeAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CurriculumVitaeAPI.Migrations
 {
     [DbContext(typeof(CVDBContext))]
-    partial class CVDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230720160545_addSummary")]
+    partial class addSummary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
